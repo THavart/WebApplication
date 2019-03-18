468f378dd1171adf0be87bb6f44b34ba045d416c
Taylor Havart-Labrecque <TH.Labrecque5@gmail.com>
2019-03-17 23:25:49 -0400

    Added coming soon page, directories tidied up.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'crispy_forms',
 ]
 
